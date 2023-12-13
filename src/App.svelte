8e76bb608dc69606805eaa6625d6e3dbcac9370f
Mark Vargas <mav49er@gmail.com>
2023-12-12 21:11:38 -0600

    Adding poll functionality and styling polls

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,32 @@
   import Footer from "./components/Footer.svelte";
   import Tabs from "./shared/Tabs.svelte";
   import CreatePollForm from "./components/CreatePollForm.svelte";
+  import PollList from "./components/PollList.svelte";
 
   // tabs
   let items = ["Current Polls", "Add New Poll"];
   let activeItem = "Current Polls";
+
   const tabChange = e => {
     activeItem = e.detail;
+  }
+
+  let polls = [
+    { 
+      id: 1,
+      question: "Python or Javascript?",
+      answerA: "Python",
+      answerB: "Javascript",
+      votesA: 9,
+      votesB: 15
+    }
+  ];
+
+  const handleAdd = (e) => {
+    const poll = e.detail;
+    polls = [poll, ...polls];
+    console.log(polls);
+    activeItem = "Current Polls";
   }
 </script>
 
@@ -16,9 +36,9 @@
 <main>
   <Tabs {items} {activeItem} on:tabChange={tabChange}/>
   {#if activeItem === "Current Polls"}
-    <p>Poll list component goes here</p>
+    <PollList {polls} />
     {:else if activeItem === "Add New Poll"}
-    <CreatePollForm />
+    <CreatePollForm on:add={handleAdd} />
   {/if}
 </main>
 <Footer />
